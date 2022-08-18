c8e7ef6bf4933922a1abdb98ae6158cf7ba75155
nadaalaa22 <nada.elrihany@gmail.com>
2022-08-19 01:01:34 +0200

    frist commit

---
+++ lib/modules/splash/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/cupertino.dart';
+
+// class SplashScreen extends StatelessWidget {
+//   const SplashScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return AnimatedSplashScreen(
+//         splash: splash, nextScreen: nextScreen);
+//   }
+// }
+
+
+
+// import 'dart:async';
+//
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// import '../../shared/componants/componants.dart';
+// import '../login/login_screen.dart';
+//
+// class SplashScreen extends StatefulWidget  {
+//   const SplashScreen ({Key? key}) : super(key: key);
+//
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
+//
+// class _SplashScreenState extends State<SplashScreen> {
+//
+//
+//
+//
+//   // startSplashTimer()
+//   // {
+//   //   Timer(Duration(seconds: 3)  , () => {
+//   //     Navigator.of(context).push(MaterialPageRoute(builder: (_) => LoginScreen()))
+//   //
+//   //    // navigateAndFinish (context , LoginScreen())
+//   //
+//   //   });
+//   // }
+//
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Material(
+//         child: Container(
+//           padding: EdgeInsets.all(40),
+//           alignment: Alignment.center,
+//           child: Image(
+//             image: AssetImage('assets/images/logo.png'),
+//
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/shared/componants/componants.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Widget defaultbutton ({
+  double width = double.infinity,
+  Color background = Colors.green,
+  required void Function()? function,
+  required String text,
+  bool isUpperCase = true,
+  double radius =0.0
+
+})=> Container(
+  width: width,
+
+  child: MaterialButton(
+
+    child: Text(
+      isUpperCase? text.toUpperCase(): text,
+      style: TextStyle(
+        fontSize: 10.0,
+        color: Colors.white,
+      ),
+
+    ),
+    onPressed:  function ,
+
+  ),
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(radius),
+    color: background,
+  ),
+);
+
+Widget defaultTextButton({
+  required void Function()? function,
+  required String text,
+}) =>
+    TextButton(
+      onPressed: function,
+      child: Text(
+        text.toUpperCase(),
+      ),
+    );
+
+Widget defaultFormField({
+  required TextEditingController  controller,
+  required TextInputType type,
+  void Function()? onTap,
+  void Function(String)? onSubmit,
+  void Function(String)? onChange,
+  required  String? Function(String?)? validate,
+  String? label,
+  IconData? prefix ,
+  IconData? suffix,
+  Function? suffixPressed,
+  bool obscureText = false
+})=> TextFormField(
+  controller: controller,
+  onFieldSubmitted: onSubmit,
+  keyboardType: type,
+  obscureText: obscureText,
+  onTap: onTap,
+  onChanged: onChange,
+  decoration: InputDecoration(
+    hintText:label,
+    hoverColor: Colors.purple,
+    border: OutlineInputBorder() ,
+    isDense: true,
+    contentPadding: EdgeInsets.fromLTRB(0, 20,0, 0),
+    // prefixIcon: Icon(
+    //     prefix
+    // ),
+    suffixIcon:  suffix != null ? IconButton(
+        onPressed: ()
+        {
+          suffixPressed!();
+        },
+        icon: Icon(suffix)) : null ,
+  ),
+  validator: validate,
+
+
+);
+void navigateAndFinish(
+    context,
+    widget,
+    ) =>
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (context) => widget,
+      ),
+          (route) {
+        return false;
+      },
+    );
